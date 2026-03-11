f8faee50f7a69568a538d76bfa7a0f12d95af3ad
hammadProjects <hammadsarwar2200@gmail.com>
2026-03-12 02:01:47 +0500

    basic rag pipeline(indexing, retrieval, generation)

---
+++ ingestion.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+from pprint import pp
+import os
+import time
+load_dotenv()
+# To prevent hitting rate limit - process big docs in batches
+batch_size = 50
+chunk_load_size = 80 # How many chunks of the splitted docs should load
+
+# init api key & quadrant client
+# get the file path
+api_key = os.getenv("GOOGLE_API_KEY")
+filePath = Path(__file__).parent / "oop.pdf"
+quadrant_client = QdrantClient("localhost", port=6333)
+
+# load and split the pdf into chunks
+loader = PyPDFLoader(file_path=filePath)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+docs = text_splitter.split_documents(loader.load()[:chunk_load_size])
+
+# create embedder for the qdrant_db - qdrant will automatically create embeddings and store
+embeddings = GoogleGenerativeAIEmbeddings(
+    api_key = api_key,
+    model="gemini-embedding-001",
+)
+
+# Get size of embeddings of current model
+vector_size = len(embeddings.embed_query("dummy query"))
+if not quadrant_client.collection_exists("rag-genai"):
+    quadrant_client.create_collection(
+        "rag-genai", 
+        vectors_config=VectorParams(size=vector_size,distance=Distance.COSINE)
+    )
+
+# qdrant vector store init
+vector_store = QdrantVectorStore(
+    collection_name="rag-genai",
+    embedding=embeddings,
+    client=quadrant_client,
+)
+
+# for i in range(0, len(docs), batch_size):
+#     batch = docs[i:i+batch_size]
+#     vector_store.add_documents(batch)
+#     time.sleep(60)
---
+++ retrieve.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+from langchain_qdrant import QdrantVectorStore
+from ingestion import embeddings
+from google import genai
+from google.genai import types
+import os
+input_query = input("> ")
+
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+client = genai.Client(api_key=api_key)
+
+qdrant = QdrantVectorStore.from_existing_collection(
+    embedding=embeddings,
+    collection_name="rag-genai",
+    url="http://localhost:6333",
+)
+
+searchResult = qdrant.similarity_search(input_query)
+# print(searchResult)
+
+page_contents = [res.page_content for res in searchResult]
+print(page_contents)
+
+# system_prompt = f"""
+# You are an helpful AI assistant who will help users navigating with the docs.
+# You are given a context. Which you have to consider as a source of truth. Do not add anything from your own.
+# If you have relevant data then you can refine the answers.
+
+# Context:
+# {page_contents}
+# """
+
+# config = types.GenerateContentConfig(
+#     system_instruction=system_prompt
+# )
+
+# response = client.models.generate_content(
+#     model="gemini-3-flash-preview",
+#     contents=input_query,
+#     config=config,
+# )
+
+# print(response.text)
